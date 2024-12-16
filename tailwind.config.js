aaa6f45e4aca4cf117e14a0f4694b17ea9dfbf8a
BOKEG <graceboke2019@gmail.com>
2024-12-16 17:04:11 +0300

    myfirst

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      screens: {
+        "sm":"480px"
+      }
+    },
+    // colors: {
+    //   "mycolor":"#555"
+    // }
+  },
+  plugins: [],
+}
+
